3aeae6dd518b804f3e5c736ca23953ac62782e90
Jamie Alden <jamiealden1@gmail.com>
2016-09-02 20:43:20 -0600

    Adding Quiz Objects

---
+++ js/quiz.js
@@ -0,0 +1,97 @@
+//Quiz Questions
+
+var question1 {
+	question:"When Fry discovers that he's a billionaire, what does he buy?",
+	a:"A Lucy Liu robot",
+	b:"A lifetime supply of Slurm",
+	c:"The last tin of sardines on Earth",
+	d:"The Planet Express",
+	correct:"The last tin of sardines on Earth"
+};
+
+var question2 {
+	question:"Finish this phrase: 'Morbo is pleased, but ____'",
+	a:"Angry",
+	b:"Sticky",
+	c:"Smelly",
+	d:"Confused",
+	correct:"Sticky"
+};
+
+var question3 {
+	question:"What is the only creature on Earth that Bender is sympathetic to?",
+	a:"Turtles",
+	b:"Goats",
+	c:"Cats",
+	d:"Butterflies",
+	correct:"Turtles"
+};
+
+var question4 {
+	question:"What is missing from Zapp Brannigan's uniform?",
+	a:"Boots",
+	b:"Gloves",
+	c:"Pants",
+	d:"Belt",
+	correct:"Pants"
+};
+
+var question5 {
+	question:"How is Professor Farnsworth related to Fry?",
+	a:"Great (x30) grandfather",
+	b:"Great (x30) nephew",
+	c:"2nd cousin 20 times removed",
+	d:"He isn't",
+	correct:"Great (x30) nephew"
+};
+
+var question6 {
+	question:"Who is Bender's evil twin?",
+	a:"Calculon",
+	b:"Roberto",
+	c:"Flexo",
+	d:"Robot Devil",
+	correct:"Flexo"
+};
+
+var question7 {
+	question:"What is Zoidberg's first name?",
+	a:"John",
+	b:"Phillip",
+	c:"Kevin",
+	d:"James",
+	correct:"John"
+};
+
+var question8 {
+	question:"How does the Femputer sentence Fry, Zapp, and Kif to die?",
+	a:"By firing squad",
+	b:"By snu snu",
+	c:"By dinosaur attack",
+	d:"By suicide booth",
+	correct:"By snu snu"
+};
+
+var question9 {
+	question:"What sport does Hermes compete in during the 2980 Olympics?",
+	a:"Octopus Wrestling",
+	b:"Blernsball",
+	c:"Swurling",
+	d:"Limbo",
+	correct:"Limbo"
+};	
+
+var question10 {
+	question:"What is Zoidberg a doctor of?",
+	a:"Art History",
+	b:"General Medicine",
+	c:"Podiatry",
+	d:"Physics",
+	correct:"Art History"
+};
+
+//Quiz Object
+
+var quiz {
+
+};
